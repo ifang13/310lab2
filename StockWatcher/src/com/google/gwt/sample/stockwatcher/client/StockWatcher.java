8aef174a75da1e3053d8f56257a22ec40841e8a3
ifang13 <irene-fang@live.ca>
2014-09-26 10:42:14 -0700

    im lame

--- StockWatcher/src/com/google/gwt/sample/stockwatcher/client/StockWatcher.java
+++ StockWatcher/src/com/google/gwt/sample/stockwatcher/client/StockWatcher.java
@@ -53,8 +53,7 @@ public class StockWatcher implements EntryPoint {
 	 * returns an error.
 	 */
 	private static final String SERVER_ERROR = "An error occurred while "
-			+ "attempting to contact the server. Please check your network "
-			+ "connection and try again.";
+			+ "attempting to contact the server.";
 
 	/**
 	 * Create a remote service proxy to talk to the server-side Greeting service.
